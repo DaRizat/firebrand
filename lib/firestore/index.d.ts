5c6fea7a97b1d322e3aa6f90aa9df399d3be63fe
Jack Bishop <jack.darizat@gmail.com>
2020-03-04 20:56:53 -0800

    New hooks

--- lib/firestore/index.d.ts
+++ lib/firestore/index.d.ts
@@ -1,3 +1,6 @@
 import useCollection from './hooks/useCollection';
 import useDocument from './hooks/useDocument';
-export { useCollection, useDocument };
+import useSet from './hooks/useSet';
+import useAdd from './hooks/useAdd';
+import useUpdate from './hooks/useUpdate';
+export { useCollection, useDocument, useSet, useAdd, useUpdate };
