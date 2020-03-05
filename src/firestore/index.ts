636fda6a2be5415e11e8986ae5fe622379f8e92d
Jack Bishop <jack.darizat@gmail.com>
2020-03-04 20:55:37 -0800

    Exporting new hooks

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -1,4 +1,7 @@
 import useCollection from './hooks/useCollection';
 import useDocument from './hooks/useDocument';
+import useSet from './hooks/useSet';
+import useAdd from './hooks/useAdd';
+import useUpdate from './hooks/useUpdate';
 
-export { useCollection, useDocument };
+export { useCollection, useDocument, useSet, useAdd, useUpdate };
